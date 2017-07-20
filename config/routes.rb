20ff1963420be13e6d43a1b05064d50bee4866f2
DennisDaddy <softteez95@gmail.com>
2017-07-21 00:55:29 +0300

    change routes and add blogs contoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
- root to: 'posts#index'
-  resources :posts
+ root to: 'blog/posts#index'
+
+ scope module: 'blog' do
+     resources :posts
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
