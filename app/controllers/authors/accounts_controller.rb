eb14eb91887af82f43a161e0807a69d0a12a1f0d
DennisDaddy <softteez95@gmail.com>
2017-07-27 09:47:09 +0300

    add update

--- app/controllers/authors/accounts_controller.rb
+++ app/controllers/authors/accounts_controller.rb
@@ -5,23 +5,33 @@ module Authors
 		end
 
 		def update_info
-			current_author.update(author_info_params)
+		if current_author.update(author_info_params)
+			flash[:success] = 'success'
+		else
+			flash[:warning] = 'not successful'
+		end
 			redirect_to authors_account_path
 					
 		end
 
 		def change_password
+			if current_author.valid_password?(author_password_params[:current_password])
+				 current_author.update(
+				 	password: author_password_params[:new_password],
+				 	password_confirmation: author_password_params[:new_password_confirmation]
+				 	)				
+			end
 			
 		end
 
 		private
 
 		def author_info_params
-			params.require(:author).permit(:current_password, :new_password, :new_password_confirmation)
+			params.require(:author).permit(:name, :email, :bio)
 		end
 
-		def author_password_params
-				params.require(:author).permit(:name, :email, :bio)
+		def author_password_params				
+			params.require(:author).permit(:current_password, :new_password, :new_password_confirmation)
 		end
 	end
 end
